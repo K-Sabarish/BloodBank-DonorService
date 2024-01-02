e192043f33a952e389b3a825932e760ffe951869
Anonymous <kodalisabarish93@gmail.com>
2024-01-02 12:38:34 +0530

    Initial Commit

---
+++ src/main/java/org/bloodbank/bloodbankdonorservice/BloodBankDonorServiceApplication.java
@@ -0,0 +1,13 @@
+package org.bloodbank.bloodbankdonorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankDonorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankDonorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bloodbank/bloodbankdonorservice/BloodBankDonorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bloodbank.bloodbankdonorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankDonorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
